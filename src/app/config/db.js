fdd3054b18bfec573162141cb00f0c190f31d0aa
louiscavalcante <louiscavalcante@hotmail.com>
2021-10-29 19:29:35 -0400

    Heroku [READY]

--- src/app/config/db.js
+++ src/app/config/db.js
@@ -1,9 +1,35 @@
-const { Pool } = require('pg')
+const green = require('chalk').bgGreen.black
+const red = require('chalk').bgRed
+const { Pool, Client } = require('pg')
 
-module.exports = new Pool({
-	user: 'postgres',
-	password: 'postgres',
-	host: process.env.DATABASE_URL || 'localhost',
-	port: process.env.PORT || 5432,
-	database: 'schoolmanager',
-})
+if (process.env.APP_ENV === 'development') {
+	console.log(green('----- Development -----'))
+
+	const dbLocal = new Pool({
+		user: 'postgres',
+		password: 'postgres',
+		host: 'localhost',
+		port: 5432,
+		database: 'schoolmanager',
+	})
+
+	console.log('Connected to Local Database!')
+	module.exports = dbLocal
+} else {
+	console.log(red('----- Production -----'))
+
+	const dbHeroku = new Client({
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+		idle_in_transaction_session_timeout: 600000,
+	})
+
+	dbHeroku
+		.connect()
+		.then(() => console.log('Connected to Heroku Database!'))
+		.catch(err => console.error(red('Heroku Database Connection Error:', err)))
+
+	module.exports = dbHeroku
+}
