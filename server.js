b74dbd5690a4a9213c13e8997d42dc2a20aef003
louiscavalcante <louiscavalcante@hotmail.com>
2021-10-29 20:16:34 -0400

    commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes.js')
+const methodOverride = require('method-override')
+
+const server = express()
+const PORT = process.env.PORT || 5000
+
+server.use(express.urlencoded({ extended: true }))
+server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
+
+server.set('view engine', 'html')
+
+nunjucks.configure('views', {
+	express: server,
+	autoescape: false, // Renders HTML inside variables
+	noCache: true,
+})
+
+server.listen(PORT, function () {
+	console.log(`Server is running at http://localhost:${PORT}`)
+})
